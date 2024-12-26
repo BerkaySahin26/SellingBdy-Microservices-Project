58e20f7e740cfe08ccfff03f207aefac298e6a6d
BerkaySahin26 <berkay.sahin26TR@gmail.com>
2024-12-26 19:41:01 +0300

    EventBus

---
+++ src/BuildingBlocks/EventBus/EventBus.Base/Abstraction/IEventBus.cs
@@ -0,0 +1,14 @@
+﻿using EventBus.Base.Events;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventBus.Base.Abstraction
+{
+    public interface IEventBus
+    {
+        void Publish (IntegrationEvent @event);
+    }
+}
